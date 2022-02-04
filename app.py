e7cbb380d07f064c60b5f6539622f2e3c4f84284
Eric Jolibois <em.jolibois@gmail.com>
2022-02-04 19:25:04 +0100

    bad fix

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ STOPWORD = "STOP"
 
 app = FastAPI()
 
-channel_req = 'query_def_request_channel'
-channel_resp = 'query_def_response_channel'
+REQ_CHANNEL_NAME = 'query_def_request_channel'
+RESP_CHANNEL_NAME = 'query_def_response_channel'
 
 
 @app.on_event('startup')
@@ -31,10 +31,12 @@ async def shutdown_event():
 @app.get('/')
 async def root(_id: int):
     print(f'start {datetime.now().time()} - id {_id}')
-    await app.state.pub.publish(channel_req, _id)
-    (chan,) = await app.state.sub.subscribe(channel_resp)
-    while await chan.wait_message():
-        msg = await chan.get()
+    assert isinstance(app.state.pub, aioredis.Redis)
+    await app.state.pub.publish(REQ_CHANNEL_NAME, _id)
+    (resp_chan,) = await app.state.sub.subscribe(RESP_CHANNEL_NAME)
+    assert isinstance(resp_chan, aioredis.Channel)
+    while await resp_chan.wait_message():
+        msg = await resp_chan.get()
         msg = json.loads(msg)
         if msg.get('_id') == int(_id):
             print(f'end {datetime.now().time()} - id {_id}')
--- executor.py
+++ executor.py
@@ -4,43 +4,45 @@ import json
 import aioredis
 from aiocache import Cache
 
-from app import channel_req, channel_resp
+from app import REQ_CHANNEL_NAME, RESP_CHANNEL_NAME
 
-cache = Cache(Cache.REDIS, ttl=100)
+cache = Cache(Cache.REDIS, ttl=10)
 
 
 async def get_random_number(x: int) -> int:
     import random
 
-    print("Generating number...")
+    print(f"Generating number for {x}...")
     n = random.randint(0, 1_000_000)
     await asyncio.sleep(5)
-    print("Generating number...done")
+    print(f"Generating number for {x}...done")
     return n * int(x)
 
 
-async def _get_random_number_from_cache_or_compute(_id: int, pub):
-    print('Got Message:', _id)
-    if (result_cached := await cache.get(f'key_{_id}')) is not None:
-        await pub.publish(channel_resp, message=json.dumps({"result": result_cached,"cache": True, '_id': _id}))
+async def _get_random_number_from_cache_or_compute(_id: int, pub: aioredis.Redis):
+    while (result_cached := await cache.get(f'key_{_id}')) == 'PENDING':
+        await asyncio.sleep(0.1)
+
+    if result_cached:
+        await pub.publish(RESP_CHANNEL_NAME, message=json.dumps({"result": result_cached,"cache": True, '_id': _id}))
     else:
+        await cache.set(f'key_{_id}', 'PENDING')
         result = await get_random_number(_id)
         await cache.set(f'key_{_id}', result)
-        await pub.publish(channel_resp, message=json.dumps({"result": result,"cache": False,  '_id': _id}))
+        await pub.publish(RESP_CHANNEL_NAME, message=json.dumps({"result": result,"cache": False,  '_id': _id}))
 
 
-async def reader(ch, redis):
-    while await ch.wait_message():
-        msg = await ch.get()
-        asyncio.ensure_future(_get_random_number_from_cache_or_compute(int(msg.decode()), redis))
-        await asyncio.sleep(0.1)
+async def reader(req_channel: aioredis.Channel, redis: aioredis.Redis):
+    while await req_channel.wait_message():
+        msg = await req_channel.get_json()
+        asyncio.ensure_future(_get_random_number_from_cache_or_compute(int(msg), redis))
 
 
 async def main():
     sub = await aioredis.create_redis(address="redis://localhost:6379")
     pub = await aioredis.create_redis(address="redis://localhost:6379")
-    (channel,) = await sub.subscribe(channel_req)
-    await reader(channel, pub)
+    (req_channel,) = await sub.subscribe(REQ_CHANNEL_NAME)
+    await reader(req_channel, pub)
 
 if __name__ == '__main__':
     asyncio.run(main())
